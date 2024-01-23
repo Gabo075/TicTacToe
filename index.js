c0975dbc6a29766da0bd92c9ce71917d5dacfca1
Gabo075 <110423828+Gabo075@users.noreply.github.com>
2024-01-23 17:34:43 -0500

    Fixed Prettier

--- index.js
+++ index.js
@@ -1,118 +1,111 @@
 // MVC
 
 //Model - Manages data
-const squares = Array.from(document.getElementsByClassName('square'))
-const menu = document.getElementById('menu')
-const resetBtn = document.getElementById('reset-btn')
-const menuTxt = document.getElementById('menu-text')
+const squares = Array.from(document.getElementsByClassName("square"));
+const menu = document.getElementById("menu");
+const resetBtn = document.getElementById("reset-btn");
+const menuTxt = document.getElementById("menu-text");
 
-let gameOver = false
-let numberOfTurns = 0
+let gameOver = false;
+let numberOfTurns = 0;
 
-const players = ["player1", "player2"]
+const players = ["player1", "player2"];
 
 const sets = [
-    //columns
-    [squares[0], squares[1], squares[2]],
-    [squares[3], squares[4], squares[5]],
-    [squares[6], squares[7], squares[8]],
+  //columns
+  [squares[0], squares[1], squares[2]],
+  [squares[3], squares[4], squares[5]],
+  [squares[6], squares[7], squares[8]],
 
-    //rows
-    [squares[0], squares[3], squares[6]],
-    [squares[1], squares[4], squares[7]],
-    [squares[2], squares[5], squares[8]],
+  //rows
+  [squares[0], squares[3], squares[6]],
+  [squares[1], squares[4], squares[7]],
+  [squares[2], squares[5], squares[8]],
 
-    //diagonals
-    [squares[0], squares[4], squares[8]],
-    [squares[2], squares[4], squares[6]]
-    
-]
+  //diagonals
+  [squares[0], squares[4], squares[8]],
+  [squares[2], squares[4], squares[6]],
+];
 
 function nextTurn() {
-    if (turn.includes("player1")) {
-
-        turn = players[1]
-
-    }else {
-
-        turn = players[0]
-
-    }
+  if (turn.includes("player1")) {
+    turn = players[1];
+  } else {
+    turn = players[0];
+  }
 }
 
 function checkForWin() {
-    sets.forEach(array => {
-        let firstItem = array[0].firstElementChild.textContent
-        if (checkArraySimilarity(array, firstItem)) {
-            console.log("win")
-            gameOver = true
-            gameOverDisplay()
-        }
-    })
+  sets.forEach((array) => {
+    let firstItem = array[0].firstElementChild.textContent;
+    if (checkArraySimilarity(array, firstItem)) {
+      console.log("win");
+      gameOver = true;
+      gameOverDisplay();
+    }
+  });
 }
 
 //Checks all items in array are the same, excluding being empty
 
 function checkArraySimilarity(array, firstItem) {
-    if (
-        array.every(item => item.firstElementChild.textContent != "") && 
-        array.every(item => item.firstElementChild.textContent === firstItem)) 
-        {
-            return true
-    }
-    return false
+  if (
+    array.every((item) => item.firstElementChild.textContent != "") &&
+    array.every((item) => item.firstElementChild.textContent === firstItem)
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function gameOverDisplay() {
-    console.log("gameOverDisplay")
-    menu.style.display = "flex"
-    resetBtn.textContent = "reset"
-    menuTxt.textContent = `${turn} won the game`
+  console.log("gameOverDisplay");
+  menu.style.display = "flex";
+  resetBtn.textContent = "reset";
+  menuTxt.textContent = `${turn} won the game`;
 }
 
 //View - Manages the diplay
 function addPlay(firstChild) {
-
-    if (turn.includes("player1")) {
-
-        firstChild.textContent = "x"
-
-    }else {
-
-        firstChild.textContent = "o"
-
-    }
+  if (turn.includes("player1")) {
+    firstChild.textContent = "x";
+  } else {
+    firstChild.textContent = "o";
+  }
 }
 
 //Controller - Controls intreractions between view and model
-let turn = players[0]
+let turn = players[0];
 
-squares.forEach(square => { square.addEventListener("click", function() {
-        squaredClicked(square) 
-    }) 
-})
+squares.forEach((square) => {
+  square.addEventListener("click", function () {
+    squaredClicked(square);
+  });
+});
 
-resetBtn.addEventListener("click", resetGame)
+resetBtn.addEventListener("click", resetGame);
 
 function squaredClicked(square) {
-    let firstChild = square.firstElementChild
+  let firstChild = square.firstElementChild;
 
-    if (firstChild.textContent === "" && !gameOver) {
-        addPlay(firstChild)
+  if (firstChild.textContent === "" && !gameOver) {
+    addPlay(firstChild);
 
-        checkForWin()
+    checkForWin();
 
-        nextTurn()
-    }
+    nextTurn();
+  }
 }
 
 function resetGame() {
-    gameOver = false
-    numberOfTurns = 0
-    turn = players[0]
+  gameOver = false;
+  numberOfTurns = 0;
+  turn = players[0];
 
-    squares.forEach(square => {square.firstElementChild.textContent = ""})
+  squares.forEach((square) => {
+    square.firstElementChild.textContent = "";
+  });
 
-    menu.style.display = "none"
-    resetBtn.textContent = ""
+  menu.style.display = "none";
+  resetBtn.textContent = "";
 }
